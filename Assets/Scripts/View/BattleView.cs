f0dbaf5d3f03bba48b56fa2cdd7e4621118f332b
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-11-24 16:52:44 +0100

    yftjiftuyhjtyugj

---
+++ Assets/Scripts/View/BattleView.cs
@@ -0,0 +1,17 @@
+﻿//@Author: Teodor Tysklind / FutureGames / Teodor.Tysklind@FutureGames.nu
+
+using UnityEngine;
+
+public class BattleView : MonoSingleton<BattleView>
+{
+    [SerializeField] private Vector2 PlayerObjectDefaultPosition;
+    [SerializeField] private Vector2 EnemyObjectDefaultPosition;
+    
+    [SerializeField] private Sprite PlayerHealthBar;
+    [SerializeField] private Sprite EnemyHealthBar;
+    [SerializeField] private Sprite DialogBox;
+    [SerializeField] private Sprite Cursor;
+    
+    private Sprite CurrentPlayerObjectSprite;
+    private Sprite CurrentEnemyObjectSprite;
+}
